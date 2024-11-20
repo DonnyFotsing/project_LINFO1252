594792952a982773412ace954eadf8fc3b0cc0fc
DonnyFotsing <161767502+DonnyFotsing@users.noreply.github.com>
2024-11-20 09:48:28 +0100

    Update philosphers.bash

--- philosphers.bash
+++ philosphers.bash
@@ -2,7 +2,7 @@
 echo "Nb_threads,Measure_1,Measure_2,Measure_3,Measure_4,Measure_5" > measures.csv
 
 # Compilation
-gcc -o philosopher projet1.c -pthread || { echo "Compilation failed!"; exit 1; }
+gcc -o philosopher philosophers.c -pthread || { echo "Compilation failed!"; exit 1; }
 for i in 2 4 8 16 32; do
     echo -n "Threads: $i"
     echo -n "$i," >> measures.csv
