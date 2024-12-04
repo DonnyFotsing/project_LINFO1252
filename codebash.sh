b09c2b1b406d0d29ac67a46b6a24cb3141709c07
DonnyFotsing <161767502+DonnyFotsing@users.noreply.github.com>
2024-12-04 08:42:31 +0100

    Add files via upload

---
+++ codebash.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+echo "Nb_threads,Measure_1,Measure_2,Measure_3,Measure_4,Measure_5" > measures.csv
+
+# Compilation
+gcc -o philosopher projet1.c -pthread || { echo "Compilation failed!"; exit 1; }
+for i in 2 4 8 16 32; do
+    echo -n "Threads: $i"
+    echo -n "$i," >> measures.csv
+    for _ in 1 2 3 4 5; do
+        TIME=$( { time ./philosopher $i > /dev/null; } 2>&1 | grep real | awk '{print $2}')
+        echo -n "$TIME;" >> measures.csv
+    done
+    echo "" >> measures.csv
+done
+
+# Message de fin
+echo "Mesures collectées et enregistrées dans measures.csv"
